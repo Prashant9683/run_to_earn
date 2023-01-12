01304953655b9d80f4c5ebf5886ba7eac43e9549
Prashant9683 <amenu4elc21048@am.students.amrita.edu>
2023-01-12 22:20:44 +0530

    added model for user

--- run_to_earn/user/admin.py
+++ run_to_earn/user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from user.models import User
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'first_name', 'last_name', 'phone', 'created_at', 'updated_at', 'deleted_at')
+    list_display_links = ('id', 'email', 'first_name', 'last_name', 'created_at')
+    search_fields = ('email', 'first_name', 'last_name',)
--- run_to_earn/user/models.py
+++ run_to_earn/user/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.password_validation import validate_password
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(
+        unique=True,
+        verbose_name='Email Address'
+    )
+    first_name = models.CharField(
+        max_length=50,
+        default='',
+        blank=True,
+        verbose_name='First Name',
+        validators=[
+            MaxLengthValidator(50)
+        ]
+    )
+    last_name = models.CharField(
+        max_length=50,
+        default='',
+        blank=True,
+        verbose_name='Last Name',
+        validators=[
+            MaxLengthValidator(50)
+        ]
+    )
+
+    phone = PhoneNumberField(
+        null=True, blank=True,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    def set_password(self, raw_password):
+        validate_password(password=raw_password, user=self)
+        return super().set_password(raw_password)
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.email
